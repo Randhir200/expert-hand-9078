1b07b0c9124227103200b3d5bb74afe57df11c6f
Randhir200 <mail.randhirkr@gmail.com>
2022-08-24 23:28:14 +0530

    working on design

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import Tracking from './pages/Features/disco';
 import Timetracker from './pages/Features/Timetracker';
 function App() {
   return (
-    <div className='App'>
-      <Timetracker />
-      <Tracking />
-    </div>
-  );
+        <div className="App">
+          <Timetracker />
+          <Tracking/>
+        </div>
+   );
 }
 export default App;
